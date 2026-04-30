722f2d3423213581fca604d3d8c04671c97a3000
pkumar1618 <prashanthch943@gmail.com>
2026-04-30 10:38:59 +0530

    chages the username and password

--- 05.varible-read.sh
+++ 05.varible-read.sh
@@ -2,6 +2,12 @@
 
 echo "Please enter  your username::"
 
-read USERNAME   # takes input into USENAME variable
+read -s USERNAME   # takes input into USENAME variable
 
 echo "Username entered is: $USERNAME"
+
+echo "Please enter your password"
+
+read -s PASSWORD 
+
+echo "Password entered is: $PASSWORD"
