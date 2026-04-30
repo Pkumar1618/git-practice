f58f472a3ba0c99f9d03a92bcfe6108cd0bf8ff6
pkumar1618 <prashanthch943@gmail.com>
2026-04-30 16:27:18 +0530

    nginx installation

--- 14-mysqlinstallation.sh
+++ 14-mysqlinstallation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PACKAGE1="nginx"
-PACKAGE2="mysql"
+PACKAGE2="mysql-server"
 if dpkg -s $PACKAGE1 &> /dev/null;
 then
    echo "$PACKAGE1 is already installed"
@@ -16,5 +16,5 @@ then
 else
    echo "$PACKAGE2 is not installed. installing.."
    apt update
-   apt install mysql -y
+   apt install mysql-server -y
 fi
